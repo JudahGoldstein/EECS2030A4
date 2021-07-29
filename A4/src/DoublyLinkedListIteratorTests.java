8f74449d5fa625fe69fb7a88a30262ea1ce2ece6
Judah Goldstein <39227048+JudahGoldstein@users.noreply.github.com>
2021-07-29 17:23:30 -0400

    made class for testing iterator

--- A4/src/DoublyLinkedListIteratorTests.java
+++ A4/src/DoublyLinkedListIteratorTests.java
@@ -2,4 +2,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class DoublyLinkedListIteratorTests {
+    @Test
+    void hasNext1(){
+        Assertions.assertNull(null);
+    }
 }
