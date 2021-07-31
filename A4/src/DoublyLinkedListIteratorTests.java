1b02103a3e8feebb91f640d3db8b2910642183dd
Rafsun Masud <rafsun82@my.yorku.ca>
2021-07-31 16:06:48 -0400

    added list iterator tests

--- A4/src/DoublyLinkedListIteratorTests.java
+++ A4/src/DoublyLinkedListIteratorTests.java
@@ -1,9 +1,226 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ListIterator;
+import java.util.NoSuchElementException;
+
 public class DoublyLinkedListIteratorTests {
     @Test
-    void hasNext1() {
-        Assertions.assertNull(null);
+    public void TestHasNext() {
+        ListIterator iterator = getSampleList().listIterator(0);
+        Assertions.assertEquals(true, iterator.hasNext());
+        iterator.next();
+        Assertions.assertEquals(true, iterator.hasNext());
+        iterator.next();
+        Assertions.assertEquals(true, iterator.hasNext());
+        iterator.next();
+        Assertions.assertEquals(true, iterator.hasNext());
+        iterator.next();
+        Assertions.assertEquals(true, iterator.hasNext());
+        iterator.next();
+        Assertions.assertEquals(false, iterator.hasNext());
+    }
+
+    @Test
+    public void TestNext() {
+        ListIterator iterator = getSampleList().listIterator(0);
+        Assertions.assertEquals(1, iterator.next());
+        Assertions.assertEquals(2, iterator.next());
+        Assertions.assertEquals(3, iterator.next());
+        Assertions.assertEquals(4, iterator.next());
+        Assertions.assertEquals(5, iterator.next());
+        Assertions.assertThrows(NoSuchElementException.class, () -> iterator.next());
+    }
+
+    @Test
+    public void TestHasPrevious() {
+        ListIterator iterator = getSampleList().listIterator(5);
+        
+        Assertions.assertEquals(true, iterator.hasPrevious());
+        iterator.previous();
+        Assertions.assertEquals(true, iterator.hasPrevious());
+        iterator.previous();
+        Assertions.assertEquals(true, iterator.hasPrevious());
+        iterator.previous();
+        Assertions.assertEquals(true, iterator.hasPrevious());
+        iterator.previous();
+        Assertions.assertEquals(true, iterator.hasPrevious());
+        iterator.previous();
+        Assertions.assertEquals(false, iterator.hasPrevious());
+    }
+
+    @Test
+    public void TestPrevious() {
+        ListIterator iterator = getSampleList().listIterator(5);
+        
+        Assertions.assertEquals(5, iterator.previous());
+        Assertions.assertEquals(4, iterator.previous());
+        Assertions.assertEquals(3, iterator.previous());
+        Assertions.assertEquals(2, iterator.previous());
+        Assertions.assertEquals(1, iterator.previous());
+        Assertions.assertThrows(NoSuchElementException.class, () -> iterator.previous());
+    }
+
+    @Test
+    public void TestNextIndex() {
+        ListIterator iterator = getSampleList().listIterator(0);
+
+        Assertions.assertEquals(0, iterator.nextIndex());
+        iterator.next();
+        Assertions.assertEquals(1, iterator.nextIndex());
+        iterator.next();
+        Assertions.assertEquals(2, iterator.nextIndex());
+        iterator.next();
+        Assertions.assertEquals(3, iterator.nextIndex());
+        iterator.next();
+        Assertions.assertEquals(4, iterator.nextIndex());
+        iterator.next();
+        Assertions.assertEquals(5, iterator.nextIndex());
+    }
+
+    @Test
+    public void TestPreviousIndex() {
+        ListIterator iterator = getSampleList().listIterator(5);
+
+        Assertions.assertEquals(4, iterator.previousIndex());
+        iterator.previous();
+        Assertions.assertEquals(3, iterator.previousIndex());
+        iterator.previous();
+        Assertions.assertEquals(2, iterator.previousIndex());
+        iterator.previous();
+        Assertions.assertEquals(1, iterator.previousIndex());
+        iterator.previous();
+        Assertions.assertEquals(0, iterator.previousIndex());
+        iterator.previous();
+        Assertions.assertEquals(-1, iterator.previousIndex());
+    }
+
+    @Test
+    public void TestRemoveHead() {
+        DoublyLinkedList<Integer> list = getSampleList();
+        ListIterator<Integer> iterator = list.listIterator(0);
+
+        Integer futureHead = list.get(1);
+        iterator.next();
+        iterator.remove();
+        Assertions.assertEquals(futureHead, list.get(0));
+    }
+
+    @Test
+    public void TestRemove() {
+        DoublyLinkedList<Integer> list = getSampleList();
+        ListIterator<Integer> iterator = list.listIterator(2);
+
+        iterator.next();
+        iterator.remove();
+
+        iterator = list.listIterator(0);
+
+        Assertions.assertEquals(4, list.size());
+        Assertions.assertEquals(1, iterator.next());
+        Assertions.assertEquals(2, iterator.next());
+        Assertions.assertEquals(4, iterator.next());
+        Assertions.assertEquals(5, iterator.next());
+    }
+
+    @Test
+    public void TestRemoveException() {
+        DoublyLinkedList<Integer> list = getSampleList();
+        ListIterator<Integer> iterator = list.listIterator(2);
+
+        // calling remove without calling next/previous
+        Assertions.assertThrows(IllegalStateException.class, () -> iterator.remove());
+
+        // calling remove after calling next
+        iterator.next();
+        Assertions.assertDoesNotThrow(() -> iterator.remove());
+
+        // calling remove again after calling remove
+        Assertions.assertThrows(IllegalStateException.class, () -> iterator.remove());
+
+        // calling remove after calling previous
+        iterator.previous();
+        Assertions.assertDoesNotThrow(() -> iterator.remove());
+
+        // calling remove after calling add
+        iterator.add(9);
+        Assertions.assertThrows(IllegalStateException.class, () -> iterator.remove());
+    }
+
+    @Test
+    public void TestSet() {
+        DoublyLinkedList<Integer> list = getSampleList();
+        ListIterator<Integer> iterator = list.listIterator(2);
+
+        iterator.next();
+
+        Assertions.assertEquals(3, list.get(2));
+        iterator.set(9);
+        Assertions.assertEquals(9, list.get(2));
+    }
+
+    @Test
+    public void TestSetException() {
+        DoublyLinkedList<Integer> list = getSampleList();
+        ListIterator<Integer> iterator = list.listIterator(2);
+
+        // calling set without calling next/previous
+        Assertions.assertThrows(IllegalStateException.class, () -> iterator.set(9));
+
+        // calling set after calling next
+        iterator.next();
+        Assertions.assertDoesNotThrow(() -> iterator.set(9));
+
+        // calling set again after calling set
+        Assertions.assertDoesNotThrow(() -> iterator.set(9));
+
+        // calling set after calling previous
+        iterator.previous();
+        Assertions.assertDoesNotThrow(() -> iterator.set(9));
+
+        // calling set after calling add
+        iterator.add(9);
+        Assertions.assertThrows(IllegalStateException.class, () -> iterator.set(9));
+    }
+
+    @Test
+    public void TestAdd() {
+        DoublyLinkedList<Integer> list = getSampleList();
+        ListIterator<Integer> iterator = list.listIterator(2);
+
+        iterator.next();
+        iterator.add(9);
+
+        iterator = list.listIterator(0);
+
+        Assertions.assertEquals(1, iterator.next());
+        Assertions.assertEquals(2, iterator.next());
+        Assertions.assertEquals(3, iterator.next());
+        Assertions.assertEquals(9, iterator.next());
+        Assertions.assertEquals(4, iterator.next());
+        Assertions.assertEquals(5, iterator.next());
+    }
+
+    @Test
+    public void TestAddHead() {
+        DoublyLinkedList<Integer> list = getSampleList();
+        ListIterator<Integer> iterator = list.listIterator(0);
+
+        Integer futureHead = 9;
+        iterator.add(futureHead);
+        Assertions.assertEquals(futureHead, list.get(0));
+    }
+
+    /**
+     * Returns a DoublyLinkedList: [1, 2, 3, 4, 5].
+     */
+    public DoublyLinkedList<Integer> getSampleList() {
+        DoublyLinkedList<Integer> list = new DoublyLinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        return list;
     }
 }
