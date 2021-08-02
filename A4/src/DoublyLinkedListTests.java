8c8f53809cf6e0de9f024c7cafb1a9e02de6c2d4
Judah Goldstein <39227048+JudahGoldstein@users.noreply.github.com>
2021-08-02 15:53:05 -0400

    redundant imports

--- A4/src/DoublyLinkedListTests.java
+++ A4/src/DoublyLinkedListTests.java
@@ -1,11 +1,6 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-import java.util.NoSuchElementException;
-
 public class DoublyLinkedListTests {
     /**
      * variables types check
